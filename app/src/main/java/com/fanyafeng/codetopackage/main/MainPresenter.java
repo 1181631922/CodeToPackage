c51f85df2e495bf52ed90bb98e5bbc3140e36ebd
fanyafeng <fanyafeng@365rili.com>
2016-12-20 18:53:59 +0800

    初步完成一套代码多套包

--- app/src/main/java/com/fanyafeng/codetopackage/main/MainPresenter.java
+++ app/src/main/java/com/fanyafeng/codetopackage/main/MainPresenter.java
@@ -4,12 +4,20 @@ import android.content.Context;
 
 import com.fanyafeng.codetopackage.R;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * Author： fanyafeng
  * Data： 16/12/20 下午6:07
  * Email: fanyafeng@live.cn
  */
 public class MainPresenter implements MainContract.Presenter {
+    private String myPackage;
+    private String myName;
+    private String myGitHub;
+    private String myCSDN;
 
     private Context context;
     private MainContract.View view;
@@ -21,18 +29,27 @@ public class MainPresenter implements MainContract.Presenter {
 
     @Override
     public void start() {
-
+        try {
+            Properties properties = new Properties();
+            InputStream inputStream = context.getAssets().open("config.properties");
+            properties.load(inputStream);
+            myPackage = properties.getProperty("mypackage");
+            myName = properties.getProperty("myname");
+            myGitHub = properties.getProperty("mygithub");
+            myCSDN = properties.getProperty("mycsdn");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void setMyPackageName() {
-        String myPackageName = "我的";
-        view.setMyPackageName(myPackageName);
+        view.setMyPackageName(myPackage);
     }
 
     @Override
     public void setMyPackageInfo() {
-        view.setMyPackageInfo("");
+        view.setMyPackageInfo(myName);
     }
 
     @Override
